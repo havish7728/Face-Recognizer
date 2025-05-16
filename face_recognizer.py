fb4cacb49113c35bdbc46540ee2e497ae70e8bc6
Havish7728 <havish7728@gmail.com>
2025-05-16 21:18:29 +0530

    Initial commit with dependencies and .gitignore for venv

---
+++ face_recognizer.py
@@ -0,0 +1,16 @@
+import boto3
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+AWS_ACCESS_KEY = os.getenv("AWS_ACCESS_KEY")
+AWS_SECRET_KEY = os.getenv("AWS_SECRET_KEY")
+AWS_REGION = os.getenv("AWS_REGION")
+
+rekognition = boto3.client(
+    'rekognition',
+    aws_access_key_id=AWS_ACCESS_KEY,
+    aws_secret_access_key=AWS_SECRET_KEY,
+    region_name=AWS_REGION
+)
